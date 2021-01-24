58d6dd6e30227fa61b7b9e1516bda48a3b0987f3
rottenkitkat <williyantohuang97@yahoo.com>
2021-01-24 10:55:34 +0700

    added alert component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import TodoListContainer from "./TodoListContainer";
 import { DispatchContextProvider } from "./context/TodosContext";
 import "@fortawesome/fontawesome-free/css/all.css";
+import Alert from "./Alert";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <DispatchContextProvider>
+        <Alert />
         <TodoListContainer />
       </DispatchContextProvider>
     </div>
